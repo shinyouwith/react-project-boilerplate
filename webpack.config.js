7dbfd001dbaa41171352dd13d1e493714763cb6d
Shinyou Park <shinyouwith@gmail.com>
2019-09-25 23:39:08 +0900

    env: ES6, React, SCSS 기반의 프로젝트를 빌드하기 위한 webpack 빌드 설정 및 템플릿 파일 추가
    - js, jsx 파일을 Babel로 컴파일-ES6, react 지원을 위한 프리셋 추가
    - scss 스타일을 css로 컴파일
    - 컴파일 된 js, css를 템플릿 html에 삽입
    - webpack-dev-server 관련 설정 추가

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./dist/"),
+    port: 9000
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // webpack아, .js나 .jsx로 끝나는 파일을 찾으면 이 설정을 적용해줘
+        exclude: /node_modules/, // 근데 /node_modules/ 하위에서는 찾지 말고.
+        loader: "babel-loader", // 찾으면 babel-loader를 통해서 불러올거야
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"] // babel-loader에는 이 설정들을 적용해줘
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./static/index.html"),
+      filename: path.join(__dirname, "./dist/index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
+};
